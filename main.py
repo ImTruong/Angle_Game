171078f47f5eaaadd636daf663ab2ff0561727fc
Nguyễn Khắc Trường <54260445+ImTruong@users.noreply.github.com>
2024-11-15 07:49:14 +0700

    Update game name main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from menu import start_menu
 from game_loop import main_game_loop
 
 pygame.init()
-pygame.display.set_caption("Gunny")
+pygame.display.set_caption("Angle_game")
 
 start_menu()
 main_game_loop()
